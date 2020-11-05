7db866dc84a6beefe3bed614e469c567b1406602
shuyiz666 <shuyiz666@bu.edu>
2020-11-05 12:37:03 -0500

    cart effect
    1. cart shake effect after product  added to cart
    2. show number of items in cart at right top of cart

--- moo-dairy/js/cart.js
+++ moo-dairy/js/cart.js
@@ -1,5 +1,6 @@
 var num = 0;
 
+// fly to cart 
 function addCart(event) {
 	var cart = $('#cart');
 	var s_left =event.clientX;
@@ -9,6 +10,7 @@ function addCart(event) {
 	var _this =$(event.target);
 	var img = _this.prev().siblings("img").attr("src");
 	var flyer = $("<img src='"+img+"' width='50' style='border-radius:50%'/>");
+	var num_cart = document.getElementById("lblCartCount");
 	flyer.css({
 		'z-index':900
 		});
@@ -26,9 +28,16 @@ function addCart(event) {
 			flyer.fadeOut("slow",function(){
 				$(this).remove();
 			});
-		}
+		},
 	});
+	//cart shake
+	setTimeout(function () {
+		cart.effect('shake', { times: 2 }, 200);
+	}, 800);
 	
-	num++;  
-	$('#cart').text(num);
+	//number in cart
+	num++;
+	num_cart.style.display='inline';  
+
+	$('#lblCartCount').text(num);		
 }
